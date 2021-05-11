e536b63eb5f85c41b559ec21ddbcc89ea4b92b2b
Hubert Yang <offspring7890@yahoo.com>
2021-05-11 09:22:48 -0700

    vercel.json

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6d51c9d5356441fdd02d2e2847aece6.js"
+  "/precache-manifest.20517cd1b60721f1584ab989e23d2786.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
